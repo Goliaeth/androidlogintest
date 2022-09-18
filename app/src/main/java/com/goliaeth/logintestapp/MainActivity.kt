756df68d5716c7b96dadc9d52c29da8306ff2413
Goliaeth <75118259+Goliaeth@users.noreply.github.com>
2022-09-18 21:53:01 +0500

    add navigation and navHostFragment

--- app/src/main/java/com/goliaeth/logintestapp/MainActivity.kt
+++ app/src/main/java/com/goliaeth/logintestapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.goliaeth.logintestapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.goliaeth.logintestapp.auth.AuthActivity
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        finish()
+        startActivity(Intent(this, AuthActivity::class.java))
     }
 }
