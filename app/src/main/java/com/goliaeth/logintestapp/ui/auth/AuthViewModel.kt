90e607a35819f310cf1f82969aa3708a21c5c749
Goliaeth <75118259+Goliaeth@users.noreply.github.com>
2022-09-19 09:56:15 +0500

    add ViewModel to use in fragment to communicate with repository

---
+++ app/src/main/java/com/goliaeth/logintestapp/ui/auth/AuthViewModel.kt
@@ -0,0 +1,10 @@
+package com.goliaeth.logintestapp.ui.auth
+
+import androidx.lifecycle.ViewModel
+import com.goliaeth.logintestapp.repository.AuthRepository
+
+class AuthViewModel(
+    private val repository: AuthRepository
+): ViewModel() {
+
+}
