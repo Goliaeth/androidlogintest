a7bd533eac1c0b0d484f488fa1cebd2b58ea245f
Goliaeth <75118259+Goliaeth@users.noreply.github.com>
2022-09-19 14:55:57 +0500

    remove onActivityCreated (is deprecated), add ActivityLifecycleObserver

--- app/src/main/java/com/goliaeth/logintestapp/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/goliaeth/logintestapp/ui/auth/LoginFragment.kt
@@ -1,23 +1,20 @@
 package com.goliaeth.logintestapp.ui.auth
 
-import android.os.Bundle
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.lifecycleScope
-import com.goliaeth.logintestapp.databinding.FragmentLoginBinding
 import com.goliaeth.logintestapp.data.network.AuthAPI
 import com.goliaeth.logintestapp.data.network.Resource
 import com.goliaeth.logintestapp.data.repository.AuthRepository
+import com.goliaeth.logintestapp.databinding.FragmentLoginBinding
 import com.goliaeth.logintestapp.ui.base.BaseFragment
 import com.goliaeth.logintestapp.ui.enable
 import com.goliaeth.logintestapp.ui.home.HomeActivity
 import com.goliaeth.logintestapp.ui.startNewActivity
 import com.goliaeth.logintestapp.ui.visible
-import kotlinx.coroutines.launch
 
 
 /**
@@ -27,38 +24,37 @@ import kotlinx.coroutines.launch
  */
 class LoginFragment : BaseFragment<AuthViewModel, FragmentLoginBinding, AuthRepository>() {
 
-    //@todo onActivityCreated is deprecated, see LifecycleObserver for the activity Lifecycle
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        binding.progressbar.visible(false)
-        binding.loginButton.enable(false)
-
-        viewModel.loginResponse.observe(viewLifecycleOwner) {
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        activity?.lifecycle?.addObserver(ActivityLifeCycleObserver {
             binding.progressbar.visible(false)
-            when (it) {
-                is Resource.Success -> {
+            binding.loginButton.enable(false)
+
+            viewModel.loginResponse.observe(viewLifecycleOwner) {
+                binding.progressbar.visible(false)
+                when (it) {
+                    is Resource.Success -> {
                         viewModel.saveAuthToken(it.value.token)
                         requireActivity().startNewActivity(HomeActivity::class.java)
-                }
-                is Resource.Failure -> {
-                    Toast.makeText(requireContext(), "Login failure", Toast.LENGTH_SHORT).show()
+                    }
+                    is Resource.Failure -> {
+                        Toast.makeText(requireContext(), "Login failure", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
-        }
 
-        binding.passwordTextEdit.addTextChangedListener {
-            val email = binding.loginTextEdit.text.toString().trim()
-            binding.loginButton.enable(email.isNotEmpty() && it.toString().isNotEmpty())
-        }
+            binding.passwordTextEdit.addTextChangedListener {
+                val email = binding.loginTextEdit.text.toString().trim()
+                binding.loginButton.enable(email.isNotEmpty() && it.toString().isNotEmpty())
+            }
 
-        binding.loginButton.setOnClickListener {
-            val email = binding.loginTextEdit.text.toString().trim()
-            val password = binding.passwordTextEdit.text.toString().trim()
-            binding.progressbar.visible(true)
-            viewModel.login(email, password)
-        }
-
+            binding.loginButton.setOnClickListener {
+                val email = binding.loginTextEdit.text.toString().trim()
+                val password = binding.passwordTextEdit.text.toString().trim()
+                binding.progressbar.visible(true)
+                viewModel.login(email, password)
+            }
+        })
     }
 
     override fun getViewModel() = AuthViewModel::class.java
