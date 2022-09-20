6ddedc672a6515820f0645545a3cca2d9f0ec934
Goliaeth <75118259+Goliaeth@users.noreply.github.com>
2022-09-20 10:27:11 +0500

    add errors handling

--- app/src/main/java/com/goliaeth/logintestapp/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/goliaeth/logintestapp/ui/auth/LoginFragment.kt
@@ -1,57 +1,56 @@
 package com.goliaeth.logintestapp.ui.auth
 
-import android.content.Context
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
-import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.goliaeth.logintestapp.data.network.AuthAPI
 import com.goliaeth.logintestapp.data.network.Resource
 import com.goliaeth.logintestapp.data.repository.AuthRepository
 import com.goliaeth.logintestapp.databinding.FragmentLoginBinding
 import com.goliaeth.logintestapp.ui.base.BaseFragment
 import com.goliaeth.logintestapp.ui.enable
+import com.goliaeth.logintestapp.ui.handleApiError
 import com.goliaeth.logintestapp.ui.home.HomeActivity
 import com.goliaeth.logintestapp.ui.startNewActivity
 import com.goliaeth.logintestapp.ui.visible
+import kotlinx.coroutines.launch
 
 class LoginFragment : BaseFragment<AuthViewModel, FragmentLoginBinding, AuthRepository>() {
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        activity?.lifecycle?.addObserver(ActivityLifeCycleObserver {
-            binding.progressbar.visible(false)
-            binding.loginButton.enable(false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.progressbar.visible(false)
+        binding.loginButton.enable(false)
 
-            viewModel.loginResponse.observe(viewLifecycleOwner) {
-                binding.progressbar.visible(false)
-                when (it) {
-                    is Resource.Success -> {
+        viewModel.loginResponse.observe(viewLifecycleOwner) {
+            binding.progressbar.visible(it is Resource.Loading)
+            when (it) {
+                is Resource.Success -> {
+                    lifecycleScope.launch {
                         viewModel.saveAuthToken(it.value.token)
                         requireActivity().startNewActivity(HomeActivity::class.java)
                     }
-                    is Resource.Failure -> {
-                        Toast.makeText(requireContext(), "Login failure", Toast.LENGTH_SHORT).show()
-                    }
-                    is Resource.Loading -> {
-                        TODO("fffff")
-                    }
+                }
+                is Resource.Failure -> handleApiError(it)
+                is Resource.Loading -> {
+                    binding.loginButton.enable(true)
                 }
             }
+        }
 
-            binding.passwordTextEdit.addTextChangedListener {
-                val email = binding.loginTextEdit.text.toString().trim()
-                binding.loginButton.enable(email.isNotEmpty() && it.toString().isNotEmpty())
-            }
+        binding.passwordTextEdit.addTextChangedListener {
+            val email = binding.loginTextEdit.text.toString().trim()
+            binding.loginButton.enable(email.isNotEmpty() && it.toString().isNotEmpty())
+        }
 
-            binding.loginButton.setOnClickListener {
-                val email = binding.loginTextEdit.text.toString().trim()
-                val password = binding.passwordTextEdit.text.toString().trim()
-                binding.progressbar.visible(true)
-                viewModel.login(email, password)
-            }
-        })
+        binding.loginButton.setOnClickListener {
+            val email = binding.loginTextEdit.text.toString().trim()
+            val password = binding.passwordTextEdit.text.toString().trim()
+            viewModel.login(email, password)
+        }
     }
 
     override fun getViewModel() = AuthViewModel::class.java
